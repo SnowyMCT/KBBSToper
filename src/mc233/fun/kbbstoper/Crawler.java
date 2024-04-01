9f0b3df7c22e0177cb71c49967d515d1b0474533
SnowCherry <152802566+SnowCherryServer@users.noreply.github.com>
2024-04-01 12:26:38 +0800

    Update Crawler.java

--- src/mc233/fun/kbbstoper/Crawler.java
+++ src/mc233/fun/kbbstoper/Crawler.java
@@ -89,6 +89,7 @@ public class Crawler {
 		for (int i = 0; i < Time.size(); i++) {
 			Date date = null;
 			try {
+				System.out.println(Time.get(i))
 				date = sdfm.parse(Time.get(i));
 			} catch (ParseException e) {
 				e.printStackTrace();
